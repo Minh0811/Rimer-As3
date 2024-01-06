c16a3df511ca53710cf46d4cbe1fdf05ea785a92
minh0811 <vokhaiminh0811@gmail.com>
2024-01-07 01:47:00 +0700

    Trying to setup register

--- Back-end/controllers/authController.js
+++ Back-end/controllers/authController.js
@@ -1,8 +1,43 @@
 import bcrypt from "bcryptjs";
-import Jwt from "jsonwebtoken";
+import jwt  from "jsonwebtoken";
+
+import User from "../Models/userModel.js";
 
 const register = async (req, res) => {
-    res.json({message: "Register endpoint works."})
+   try {
+    const {email, name, password} = req.body;
+
+    if (!email || !name || !password){
+        return res.status(400).json({message: "Incomplete data."});
+    }
+
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        return res.status(400).json({message: "User already exists"});
+    }
+
+    const salt = await bcrypt.genSalt();
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    })
+
+    setJwtCookie(res, token);
+
+    res.status(201);
+    
+   } catch (error) {
+    
+   }
 
 }
 export {register};
