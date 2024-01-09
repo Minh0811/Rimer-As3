203e282e463c4524a86f4afc2a77f6bba78b7bfd
Giang2705 <95325796+Giang2705@users.noreply.github.com>
2024-01-09 21:01:25 +0700

    google authencation

--- Back-end/routes/authRoutes.js
+++ Back-end/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js";
+import { register, login, loginByGoogle, logout} from "../controllers/authController.js";
 
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/loginGoogle", loginByGoogle)
 
 export default router;
