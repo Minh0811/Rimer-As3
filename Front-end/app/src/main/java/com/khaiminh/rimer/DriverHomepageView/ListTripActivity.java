c911cf6d1fdeb74f205baceab9d62b4625852140
Truong <tantruongnguyen14@gmail.com>
2024-01-14 03:38:20 +0700

    Add booking view, fix image shape to circular

---
+++ Front-end/app/src/main/java/com/khaiminh/rimer/DriverHomepageView/ListTripActivity.java
@@ -0,0 +1,18 @@
+package com.example.rimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class ListTripActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    ListTripAdapter listTripAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
