04ea9a2ac5b9e84358adbec7b5f84f5cb599a7d2
Giang2705 <95325796+Giang2705@users.noreply.github.com>
2024-01-15 22:18:41 +0700

    fix some conflict

--- Front-end/app/src/main/java/com/khaiminh/rimer/Controllers/UserControllers/IUserControllers.java
+++ Front-end/app/src/main/java/com/khaiminh/rimer/Controllers/UserControllers/IUserControllers.java
@@ -2,11 +2,15 @@ package com.khaiminh.rimer.Controllers.UserControllers;
 
 import android.content.Context;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
+import java.util.HashMap;
+
 public interface IUserControllers {
-    void authGoogle(GoogleSignInOptions gso, GoogleSignInClient gsc);
     void login(String email, String password, Context context);
-    void signup(String name, String email, String password, Context context);
+    void signup(GoogleSignInAccount acct, String name, String email, String password, Context context);
+
+    void driverSignup(String userType, String name, String email, String password, Context context);
 }
