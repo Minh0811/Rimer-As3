5fe37fa745b833db2e0266d8688b1be8eb829e9f
Giang2705 <95325796+Giang2705@users.noreply.github.com>
2024-01-16 23:19:54 +0700

    update search map

--- Front-end/app/src/main/java/com/khaiminh/rimer/Views/UserViews/UserHomeActivity/UserHomeActivity.java
+++ Front-end/app/src/main/java/com/khaiminh/rimer/Views/UserViews/UserHomeActivity/UserHomeActivity.java
@@ -1,20 +1,41 @@
 package com.khaiminh.rimer.Views.UserViews.UserHomeActivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
@@ -22,11 +43,17 @@ import com.khaiminh.rimer.Controllers.UserControllers.UserControllers;
 import com.khaiminh.rimer.R;
 import com.khaiminh.rimer.Views.AuthenticationViews.LoginView.LoginActivity;
 
-public class UserHomeActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class UserHomeActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleSignInOptions gso;
     private GoogleSignInClient gsc;
     private UserControllers userControllers = new UserControllers();
     private DrawerLayout drawerLayout;
+    private GoogleMap mMap;
+    private final static int LOCATION_REQUEST_CODE = 23;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,13 +102,56 @@ public class UserHomeActivity extends AppCompatActivity {
         // Check for Google account and set the username
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
         if (acct != null) {
-            userControllers.signup(acct, acct.getDisplayName(), acct.getEmail(), ""+acct.getEmail()+acct.getId(), this);
+            userControllers.signup(acct, acct.getDisplayName(), acct.getEmail(), "" + acct.getEmail() + acct.getId(), this);
             String personName = acct.getDisplayName();
             navUsername.setText(personName);
         }
 
         // Set sign out button listener
         signOutBtn.setOnClickListener(v -> signOut());
+
+//        Map logic
+        SearchView mapSearchView = (SearchView) findViewById(R.id.searchField);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+        }
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null) {
+                    Geocoder geocoder = new Geocoder(UserHomeActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
     @Override
@@ -102,6 +172,65 @@ public class UserHomeActivity extends AppCompatActivity {
                 // Redirect to login activity after sign out
                 finish();
                 startActivity(new Intent(UserHomeActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+//        List<Address> addressList = null;
+//        Geocoder geocoder = new Geocoder(HomePageActivity.this);
+//
+//        for (int i = 0; i < allLocations.size(); i++){
+//            try {
+//                addressList = geocoder.getFromLocationName("Le Quang Dinh", 1);
+//            } catch (IOException e){
+//                e.printStackTrace();
+//            }
+//            Address address = addressList.get(i);
+//            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+//            mMap.addMarker(new MarkerOptions().position(latLng).title(allLocations.get(i).getName()));
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+//        }
+
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng ltlng = new LatLng(location.getLatitude(), location.getLongitude());
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+                        ltlng, 20f);
+                mMap.animateCamera(cameraUpdate);
+                mMap.getUiSettings().setZoomControlsEnabled(true);
+                mMap.getUiSettings().setCompassEnabled(true);
+            }
+        });
+        Location location = mMap.getMyLocation();
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Toast.makeText(UserHomeActivity.this, "click", Toast.LENGTH_LONG).show();
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+
+                mMap.clear();
+                CameraUpdate location = CameraUpdateFactory.newLatLngZoom(
+                        latLng, 20);
+                mMap.animateCamera(location);
+                mMap.addMarker(markerOptions);
             }
         });
     }
