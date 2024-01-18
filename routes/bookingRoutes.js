2dfa3adb7208176b126c00a609aabae526763df6
minh0811 <vokhaiminh0811@gmail.com>
2024-01-18 22:01:56 +0700

    Implement display trip list to the driver

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBooking, updateBooking, getAllBookings, getABooking, deleteBooking } from "../controllers/bookingController.js";
+import { createBooking, updateBooking, getAllBookings, getABooking, deleteBooking, getDriverBookings } from "../controllers/bookingController.js";
 
 const router = express.Router();
 
@@ -9,5 +9,5 @@ router.put("/:id", updateBooking);
 router.get("/", getAllBookings);
 router.get("/:id", getABooking);
 router.delete("/:id", deleteBooking);
-
+router.get("/driver/:driverId", getDriverBookings);
 export default router;
