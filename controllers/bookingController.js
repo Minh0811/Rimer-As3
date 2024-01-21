1ce227908f2341a458b5189aec94c864fd818e61
minh0811 <vokhaiminh0811@gmail.com>
2024-01-21 13:34:08 +0700

    update getABooking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,7 +33,9 @@ const getAllBookings = async (req, res) => {
 //GET A BOOKING
 const getABooking = async (req, res) => {
   try {
-    const booking = await Booking.findById(req.params.id).populate("user", "driver");
+    const booking = await Booking.findById(req.params.id)
+      .populate("user")
+      .populate("driver");
     res.status(200).json(booking);
   } catch (err) {
     res.status(500).json(err);
